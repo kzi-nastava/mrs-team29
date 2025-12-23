fd7054987ae3b666979ba46da9e2d9477c3ad777
RemJev269 <njevremovic01@gmail.com>
2025-12-23 23:28:50 +0100

    refactor: DriverService
    added public in front of functions

--- backend/src/main/java/service/DriverService.java
+++ backend/src/main/java/service/DriverService.java
@@ -4,6 +4,6 @@ import main.java.domain.entities.Driver;
 import main.java.domain.enums.DriverStatus;
 
 public interface DriverService {
-	Driver getAvailableDriver();
-	void updateStatus(String driverId, DriverStatus status);
+	public Driver getAvailableDriver();
+	public void updateStatus(String driverId, DriverStatus status);
 }
