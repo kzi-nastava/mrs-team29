748d5d1e1c54bd72cbd4a6078b48107272697a43
RemJev269 <njevremovic01@gmail.com>
2025-12-23 23:20:17 +0100

    feat: Initial components & pages
    Creating initial components and pages, aka register, login, profile and dirver-history and navbar

--- frontend/web/driverr-ui/src/app/app.routes.ts
+++ frontend/web/driverr-ui/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DriverHistoryComponent } from './pages/driver-history/driver-history.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'driver-history', component: DriverHistoryComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ frontend/web/driverr-ui/src/app/pages/driver-history/driver-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-history',
+  imports: [],
+  templateUrl: './driver-history.html',
+  styleUrl: './driver-history.css',
+})
+export class DriverHistory {
+
+}
