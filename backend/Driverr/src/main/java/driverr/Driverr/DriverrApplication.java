3549378c343057ffefc0fc9440e5f8449032f67d
RemJev269 <njevremovic01@gmail.com>
2025-12-23 18:25:56 +0100

    feat: Crete initial folder structure
    Create initial folder structures + Spring Boot project

---
+++ backend/Driverr/src/main/java/driverr/Driverr/DriverrApplication.java
@@ -0,0 +1,13 @@
+package driverr.Driverr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverrApplication.class, args);
+	}
+
+}
---
+++ backend/Driverr/src/test/java/driverr/Driverr/DriverrApplicationTests.java
@@ -0,0 +1,13 @@
+package driverr.Driverr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
