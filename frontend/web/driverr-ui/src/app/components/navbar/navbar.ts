80935d1c734db6630fa7f3e875ec0a6c38eab130
RemJev269 <njevremovic01@gmail.com>
2025-12-24 00:25:33 +0100

    feat: NavBar import
    imported navbar into app.ts for later

--- frontend/web/driverr-ui/src/app/components/navbar/navbar.ts
+++ frontend/web/driverr-ui/src/app/components/navbar/navbar.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   imports: [],
+  standalone: true,
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css'],
 })
 export class Navbar {
 
