47b115bb7b0330557e1572ecd0aa6fadccca06f6
RemJev269 <njevremovic01@gmail.com>
2025-12-27 19:17:57 +0100

    feat: Driver Controller
    Creating nessecary classes and packages + Driver Controller

---
+++ backend/src/main/java/dto/driver/DriverRegistrationDTO.java
@@ -0,0 +1,5 @@
+package dto.driver;
+
+public class DriverRegistrationDTO {
+
+}
--- backend/src/main/java/service/DriverService.java
+++ backend/src/main/java/service/DriverService.java
@@ -2,8 +2,10 @@ package service;
 
 import domain.entities.Driver;
 import domain.enums.DriverStatus;
+import dto.driver.*;
 
 public interface DriverService {
 	public Driver getAvailableDriver();
 	public void updateStatus(String driverId, DriverStatus status);
+	Driver registerDriver(DriverRegistrationDTO dto);
 }
