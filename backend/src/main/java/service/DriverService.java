1c32cb201283522292a51263215162cf2e91e430
RemJev269 <njevremovic01@gmail.com>
2025-12-27 19:43:45 +0100

    feat: DriverRegistrationImp
    Creating DriverRegistrationImp class where driver is created from dto data.

--- backend/src/main/java/service/DriverService.java
+++ backend/src/main/java/service/DriverService.java
@@ -7,5 +7,5 @@ import dto.driver.*;
 public interface DriverService {
 	public Driver getAvailableDriver();
 	public void updateStatus(String driverId, DriverStatus status);
-	Driver registerDriver(DriverRegistrationDTO dto);
+	public Driver registerDriver(DriverRegistrationDTO dto);
 }
--- backend/src/main/java/service/impl/DriverServiceImpl.java
+++ backend/src/main/java/service/impl/DriverServiceImpl.java
@@ -1,5 +1,54 @@
 package service.impl;
 
-public class DriverServiceImpl {
+import dto.driver.DriverRegistrationDTO;
+import domain.entities.*;
+import domain.enums.*;
+import service.DriverService;
 
+public class DriverServiceImpl implements DriverService {
+
+    @Override
+    public Driver registerDriver(DriverRegistrationDTO dto) {
+
+    	Driver driver = new Driver();
+
+        // === USER FIELDS (inherited) ===
+        driver.setFirstName(dto.getFirstName());
+        driver.setLastName(dto.getLastName());
+        driver.setGender(dto.getGender());
+        driver.setUserName(dto.getUsername());
+        driver.setEmail(dto.getEmail());
+        driver.setPassword(dto.getPassword());
+        driver.setUserType(dto.getUserType());
+        driver.setPhoneNumber(dto.getPhoneNumber());
+        driver.setAddress(dto.getAddress());
+        driver.setProfilePictureUrl(dto.getProfilePictureUrl());
+        
+
+        // === DRIVER FIELDS ===
+        Vehicle vehicle = new Vehicle();
+        vehicle.setVehicleModel(dto.getVehicleModel());
+        vehicle.setType(dto.getVehicleType());
+        vehicle.setRegistrationPlate(dto.getRegistrationPlate());
+        vehicle.setSeats(dto.getSeats());
+        vehicle.setBabiesAllowed(dto.isAllowsPets());
+        vehicle.setPetsAllowed(dto.isAllowsBabies());
+
+        driver.setVehicle(vehicle);
+        driver.setStatus(DriverStatus.AVAILABLE);
+
+        return driver;
+    }
+
+	@Override
+	public Driver getAvailableDriver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateStatus(String driverId, DriverStatus status) {
+		// TODO Auto-generated method stub
+		
+	}
 }
