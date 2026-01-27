5ffcfa5aceaaf8922e76f7b19593c4849ea6fb70
RemJev269 <njevremovic01@gmail.com>
2026-01-27 22:51:42 +0100

    feat: UserProfile frontend web update

--- frontend/web/driverr-ui/src/app/pages/profile/profile.component.ts
+++ frontend/web/driverr-ui/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
   standalone: true,
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // TODO: kasnije ovo punimo iz backend-a
+    const mockUser = {
+      firstName: 'Nenad',
+      lastName: 'Jevremović',
+      email: 'nenad@example.com',
+      username: 'nightowl',
+      phoneNumber: '+381 64 123 456'
+    };
+
+    this.form = this.fb.group({
+      firstName: [mockUser.firstName, Validators.required],
+      lastName: [mockUser.lastName, Validators.required],
+      email: [mockUser.email, [Validators.required, Validators.email]],
+      username: [mockUser.username, Validators.required],
+      phoneNumber: [mockUser.phoneNumber]
+    });
+  }
+
+  save(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('Updated profile:', this.form.value);
+
+    // TODO: userService.updateProfile(this.form.value)
+  }
 }
