880298622c81adcbe4656fbfaf245f8054f99307
RemJev269 <njevremovic01@gmail.com>
2026-01-27 12:10:29 +0100

    feat: ChangePasswordDTO

---
+++ backend/src/main/java/dto/user/ChangePasswordDTO.java
@@ -0,0 +1,12 @@
+package dto.user;
+
+public class ChangePasswordDTO {
+	private String oldPassword;
+    private String newPassword;
+
+    public String getOldPassword() { return oldPassword; }
+    public String getNewPassword() { return newPassword; }
+    
+    public void setOldPassword(String oldPassword) { this.oldPassword = oldPassword; }
+    public void setNewPassword(String newPassword) { this.newPassword = newPassword; }
+}
