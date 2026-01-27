c62c10d3d4618460ff29a597ab63c4d288588be6
RemJev269 <njevremovic01@gmail.com>
2026-01-27 10:15:59 +0100

    feat: RideRepository Interface

---
+++ backend/src/main/java/repository/RideRepository.java
@@ -0,0 +1,31 @@
+package repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.entities.*;
+import domain.enums.*;
+
+@Repository
+public interface RideRepository extends JpaRepository<Ride, String> {
+
+    List<Ride> findByStatus(RideStatus status);
+
+    List<Ride> findByDriverIdAndStatus(String driverId, RideStatus status);
+
+    List<Ride> findByPassengers_IdAndStatus(String passengerId, RideStatus status);
+
+    boolean existsByPassengers_IdAndStatusIn(
+        String passengerId,
+        List<RideStatus> statuses
+    );
+
+    Optional<Ride> findFirstByDriverIdAndStatusOrderByIdAsc(
+        String driverId,
+        RideStatus status
+    );
+}
+
