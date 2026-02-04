b0de27368b8eb364b9ac566eb082e6e3be07c7b0
RemJev269 <njevremovic01@gmail.com>
2026-02-04 20:13:15 +0100

    bugfix: SpringBootApplication edited

--- backend/src/main/java/main/DriverrApplication.java
+++ backend/src/main/java/main/DriverrApplication.java
@@ -3,9 +3,20 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"main",
+	"controller",
+	"service",
+	"repository",
+	"config",
+	"domain",
+	"dto",
+	"utils"
+})
 @EntityScan(basePackages = "domain.entities")
+@EnableJpaRepositories(basePackages = "repository")
 public class DriverrApplication {
 
 	public static void main(String[] args) {
