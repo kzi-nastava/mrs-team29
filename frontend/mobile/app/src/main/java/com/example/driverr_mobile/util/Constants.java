a34359aadb71334d80b8e8156376a8b07a62f6f8
RemJev269 <njevremovic01@gmail.com>
2026-02-11 19:28:25 +0100

    build: Mobile to Backend connection

--- frontend/mobile/app/src/main/java/com/example/driverr_mobile/util/Constants.java
+++ frontend/mobile/app/src/main/java/com/example/driverr_mobile/util/Constants.java
@@ -3,5 +3,5 @@ package com.example.driverr_mobile.util;
 public final class Constants {
     private Constants() {}
 
-    public static final String BASE_URL = "http://10.0.2.2:8081/api/";
+    public static final String BASE_URL = "http://192.168.0.4:8081/api/";
 }
