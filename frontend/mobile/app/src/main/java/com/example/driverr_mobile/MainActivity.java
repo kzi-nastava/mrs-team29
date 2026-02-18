a2f7eb7fd654ba893246d937b5bfeff96512109d
RemJev269 <njevremovic01@gmail.com>
2026-02-18 17:29:47 +0100

    bugfix: Mobile, Web Favorites Loading, Driver Ride Page in navbar

--- frontend/mobile/app/src/main/java/com/example/driverr_mobile/MainActivity.java
+++ frontend/mobile/app/src/main/java/com/example/driverr_mobile/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             startActivity(new Intent(this, FavoriteRoutesActivity.class));
             return;
         }
-        if (item.getItemId() == R.id.nav_ride) {
+        if (item.getItemId() == R.id.nav_current_ride) {
             startActivity(new Intent(this, DriverRideActivity.class));
             return;
         }
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setVisible(menu, R.id.nav_order, isClient);
         setVisible(menu, R.id.nav_favorites, isClient);
 
-        setVisible(menu, R.id.nav_ride, isDriver);
+        setVisible(menu, R.id.nav_current_ride, isDriver);
         setVisible(menu, R.id.nav_driver_history, isDriver);
 
         setVisible(menu, R.id.nav_profile, isClient || isDriver);
